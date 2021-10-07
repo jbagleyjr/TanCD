92191784eaafc31559fc2375352697918edb7bc9
jbagleyjr <james_bagley@mentor.com>
2021-10-07 12:39:17 -0700

    adding files for python setuptools

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='tancd',
+    version='0.1',    
+    description='Tanium CI/CD REST API methods',
+    url='https://github.com/jbagleyjr/TanCD',
+    author='James Bagley',
+    author_email='james_bagley@mentor.com',
+    license='Apache-2.0',
+    py_modules=["tancd","tanrest"],
+    # install_requires=['http',
+    #                   'json',
+    #                   'requests',                    
+    #                   ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache-2.0 License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
